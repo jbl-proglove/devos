954dca0976056ed6e59df78faab7f0de4e1e0b78
jbl-proglove <julius.blank@proglove.de>
2021-04-03 23:48:21 +0200

    base develop profile

--- profiles/develop/default.nix
+++ profiles/develop/default.nix
@@ -1,1 +1,18 @@
-{ ... }: { }
+{ pkgs, ... }: {
+  # TODO imports
+
+  # TODO needed?
+  # environment.shellAliases = { v = "$EDITOR"; pass = "gopass"; };
+
+  environment.sessionVariables = {
+    # PAGER = "bat";
+    LESS = "-iFJMRWX -z-4 -x4";
+    # LESSOPEN = "|${pkgs.lesspipe}/bin/lesspipe.sh %s";
+    EDITOR = "vim";
+    # VISUAL = "vim";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+}
